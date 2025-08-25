1bbac75f1e530e3225038198e85809161da0315f
NZig618 <94143570+NZig618@users.noreply.github.com>
2025-08-25 16:57:10 -0400

    [Implementation] Added scene transition logic to scene manager

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,10 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+
+    [SerializeField] public float fadeDuration;
+    public SceneFade sceneFade;
+
+    private IEnumerator Start()
+    {
+        yield return sceneFade.FadeInCoroutine(fadeDuration);
+    }
+
+    private IEnumerator LoadSceneCoroutine(string sceneToLoad)
+    {
+        yield return sceneFade.FadeOutCoroutine(fadeDuration);
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
+    }
+
     public void ChangeScene(string sceneToLoad)
     {
-        SceneManager.LoadScene(sceneToLoad);
+        StartCoroutine(LoadSceneCoroutine(sceneToLoad));
     }
 }
