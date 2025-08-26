99099bda49d21bf56eb953a45485d4c5405e0775
NZig618 <94143570+NZig618@users.noreply.github.com>
2025-08-26 17:20:13 -0400

    [Implementation] Added bullet prefab

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -7,12 +7,18 @@ public class BulletController : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     public void Explode()
     {
         // Add explosion effects here (e.g., particle effects, sound)
+        Destroy(gameObject);
+    }
+    
+    //If object goes off screen, destroy without animating
+    private void OnBecameInvisible()
+    {
         Destroy(gameObject);
     }
     
